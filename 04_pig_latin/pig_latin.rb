88715625d4b798e7c613feabe70a7993e8550ef2
Øistein Haugland <haugland.1193@gmail.com>
2017-01-25 18:07:08 +0100

    finished pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,35 @@
 #write your code here
-def translate(word)
-	newWord = ""
-	vowels = ["a","e","i","o","u","y"]
-	consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
 
+def translate(word)
+	vowels = ['a','e','i','o','u','y']
+	
+	sentence = word.split(" ")
+
+
+	sentence.each do |word|
+		temp = ''
+		until ['a','e','i','o','u'].include? (word[0])
+			if 0 < word.length - 1 && word[0] == 'q' && word[1] == 'u' then
+			  	temp << word[0,2]
+        				word[0,2] = ''
+			else
+				temp << word[0]
+        				word[0] = ''
+			end				
+		end
+		word << temp + "ay"
+	end	
+ 	sentence.join(" ")
+end
+
+
+=begin
+other atemps
+
+#consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+
+
+#first attempt, failed second to last test.
 	if word.include? " " #more than one word
 		x = word.split(" ")
 		x.each do |a|
@@ -41,4 +67,16 @@ def translate(word)
 		
 
 	end
+=end
+
+
+
+=begin
+#a solution i dont understand
+def translate(str)
+
+	vowels = ['a', 'e', 'i', 'o', 'u']	
+	s = str.split(' ').map{ |i| (vowels.include? i[0]) ? i = i + "ay" : i = i.gsub(/\b([bcdfghjklmnprstvwxyz]|qu)+/, '') + i.slice(/\b([bcdfghjklmnprstvwxyz]|qu)+/) + "ay" }.join(' ')
+	
 end
+=end
