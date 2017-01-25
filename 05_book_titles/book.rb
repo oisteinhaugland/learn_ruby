5279eb82b1212e9c9d9d676a41b74072be73e326
Øistein Haugland <haugland.1193@gmail.com>
2017-01-25 18:07:38 +0100

    finished book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+	
+	attr_reader :title
+	
+	def title= (book_name)
+		little_words = ['for', 'and', 'or', 'in', 'the', 'of' ,'a', 'an']	
+		word_array = book_name.split(" ")
+
+		word_array.each_with_index do | i , index |
+			if !little_words.include? i or index == 0
+				i.capitalize!
+			end
+		end
+		word_array = word_array.join(' ')
+		@title = word_array
+	end
 end
+
+	
