4299ac859c157363ac5653f50e918744a26edc52
Øistein Haugland <haugland.1193@gmail.com>
2017-01-25 18:05:18 +0100

    Finished timer project

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def seconds=(seconds)
+		@seconds= seconds
+	end
+
+	def time_string
+		#there are 3600 seconds in one hour
+		#the remaining minutes are seconds / by 60 - to get to minutes. If you then find the remainding minutes.
+		#find how many remaining seconds there are
+
+		hours = seconds / 3600
+		minutes = (seconds / 60) % 60 
+		rest = seconds % 60
+
+		sprintf('%02i:%02i:%02i',hours,minutes,rest) #save print with format.
+		
+	end
 end
